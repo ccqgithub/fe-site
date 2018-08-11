402062ed8378e1f54476f230e2dfd51fb7d1a09f
season.chen <season.chen.i@foxmail.com>
2018-08-12 00:11:57 +0800

    router

--- src/entry/rc-router-admin.jsx
+++ src/entry/rc-router-admin.jsx
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import { Switch, matchPath } from 'react-router';
-import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import '../less/rc-router-admin.less';
@@ -13,14 +13,41 @@ class PageDetail extends React.Component {
     
   }
   
+  render() {
+    return <div></div>
+  }
 }
 
 class Page extends React.Component {
   constructor(props) {
     super(props);
-    
+    this.time = Date.now();
   }
   
+  render() {
+    return <div className={`app-page ${this.props.className}`}>
+      <p>{this.time}</p>
+    </div>
+  }
+}
+
+class TPage extends React.Component {
+  constructor(props) {
+    super(props); 
+  }
+  
+  render() {
+    return (
+      <CSSTransition 
+        in={this.props.status == 'show'} 
+        timeout={5000} 
+        appear={true} 
+        classNames="page"
+      >
+        <Route component={this.props.component} location={this.props.location}/>
+      </CSSTransition>
+    )
+  }
 }
 
 // routes
@@ -57,47 +84,38 @@ const routes = [
   }
 ];
 
+const NavLink = ({ to, ...rest }) => (
+  <Route path={to} children={({ match }) => (
+    <li className={match ? 'active' : ''}>
+      <Link to={to} {...rest}>{to}</Link>
+    </li>
+  )}/>
+);
+
 // app
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      openPages: [],
-      activePage: props.history.pathname
+      openPages: [props.location.pathname],
+      activePage: props.location.pathname
     }
 
-    this.history = props.history;
     this.routes = routes;
+  }
 
-    this.unlisten = history.listen((location, action) => {
-      let pages = this.state.openPages;
-      switch(action) {
-        case 'POP':
-          pages = this.removePage(pages, this.state.activePage);
-          pages = this.activePage(pages, location.pathname);
-          this.setState({ 
-            openPages: pages, 
-            activePage: location.pathname
-          });
-          break;
-        case 'PUSH':
-          pages = this.activePage(pages, location.pathname);
-          this.setState({ 
-            openPages: pages, 
-            activePage: location.pathname
-          });
-          break;
-        case 'REPLACE':
-          pages = this.removePage(pages, this.state.activePage);
-          pages = this.activePage(pages, location.pathname);
-          this.setState({ 
-            openPages: pages, 
-            activePage: location.pathname
-          });
-          break;
-      }
-    });
+  static getDerivedStateFromProps(props, state) {
+    let page = props.location.pathname;
+    let pages = state.openPages;
+    let index = pages.indexOf(page);
+    if (index != -1) pages.splice(index, 1);
+    pages.push(page);
+
+    return  {
+      openPages: pages,
+      activePage: page
+    }
   }
 
   // active page
@@ -120,20 +138,33 @@ class App extends React.Component {
   }
   
   render() {
-    const NavLink = ({ to, ...rest }) => (
-      <Route path={to} children={({ match }) => (
-        <li className={match ? 'active' : ''}>
-          <Link to={to} {...rest}>{to}</Link>
-        </li>
-      )}/>
-    );
+    let pathname = this.props.location.pathname;
+    let openPages = this.state.openPages;
 
-    let transitionKey = this.props.location.pathname
-      .replace(/^\/(\w+).*$/, '/$1');
+    const pages = openPages.map(path => {
+      let match;
+      let route;
+      
+      this.routes.some(item => {
+        match = matchPath(path, {path: item.path, exact: true});
+        route = item;
+        return !!match;
+      });
+      
+      if (!match) return null;
+      
+      let matchActive = matchPath(pathname, {path: route.path, exact: true});
+      let status = matchActive ? 'show' : 'hide';
 
-    const Pages = ({ match, history, location }) => {
-
-    };
+      return (
+        <TPage 
+          key={match.url} 
+          status={status} 
+          component={route.component} 
+          location={this.props.location}>
+        </TPage>
+      )
+    });
 
     return (
       <Fragment>
@@ -149,38 +180,10 @@ class App extends React.Component {
 
           </div>
 
-          <TransitionGroup className="app-pages">
-            <CSSTransition key={transitionKey} classNames="entry-page" timeout={500}>
-              <Switch location={this.props.location}>
-                <Route path="/a" component={PageA} exact={false} />
-
-                <Route path="/b" component={PageB} exact={true} />
-                <Route path="/b/:id" component={PageBDetail} exact={true} />
-
-                <Route path="/c" component={PageC} exact={false} />
-
-                <Route render={() => {
-                  return (
-                    <div className="app-page">
-                      <p>
-                        React Router H5 Demo
-                      </p>
-                      <p>
-                        '/a': 子页面层层递进，不能单独访问
-                      </p>
-                      <p>
-                        '/b': 子页面单独访问
-                      </p>
-                      <p>
-                        '/c': 路由弹窗
-                      </p>
-                    </div>
-                  )
-                }} exact={false}/>
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-
+          <div className="app-pages">
+            {pages}           
+          </div>
+          
         </div>
       </Fragment>
     );
