4c1830087b6465fc7271c8535c8032285ee509f3
season.chen <season.chen.i@foxmail.com>
2018-08-11 15:51:50 +0800

    router

--- src/entry/rc-router-h5.jsx
+++ src/entry/rc-router-h5.jsx
@@ -2,19 +2,26 @@ import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import { Switch, matchPath } from 'react-router';
-import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import '../less/rc-router-h5.less';
 
-// get first matched url from routes 
+// get first matched routes 
 // with current location
-const getFirstUrlFromRoutes = (routes, location) => {
+const getMatchedRoute = (routes, location) => {
   let key = null;
+  let route = null;
+  let index = -1;
 
-  routes.some(item => {
+  routes.some((item, i) => {
     let match = matchPath(location.pathname, {
+      path: item.path,
+      exact: true
+    });
+
+    let keyMatch = matchPath(location.pathname, {
       path: item.path,
       exact: false
     });
@@ -25,69 +32,15 @@ const getFirstUrlFromRoutes = (routes, location) => {
     });
 
     if (match || childMatch) {
-      key = match.url;
+      key = keyMatch.url;
+      route = item;
+      index = i;
     }
 
     return !!key;
   });
 
-  return key;
-}
-
-// add transiton route to inner page
-// props: component, path, exact, location, ...other
-class InnerPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: null,
-      match: null
-    }
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    let {path, exact, location} = props;
-    let match = matchPath(location.pathname, {
-      path,
-      exact
-    });
-
-    return {
-      location: match ? location : state.location,
-      match
-    }
-  }
-
-  render() {
-    let { path, exact, component: Component } = this.props;
-
-    if (!this.state.location) return null;
-
-    return (
-      <CSSTransition 
-        in={!!this.state.match} 
-        classNames="inner-page" 
-        timeout={250} 
-        appear={true}
-        unmountOnExit={true}
-      >
-        <Route  
-          location={this.state.location}
-          path={path}
-          exact={exact}
-          render={({ match, ...rest }) => {
-            if (!match) return null;
-            return (
-              <Component 
-                {...this.props}
-                {...rest}
-              />
-            )
-          }}
-        />
-      </CSSTransition>
-    )
-  }
+  return { key, route, index };
 }
 
 // gender
@@ -149,7 +102,12 @@ class Form extends React.Component {
   }
   
   render() {
-    let url = this.props.parentPath + '/form';
+    let { match, location } = this.props;
+    let genderUrl = `${match.url}/gender`;
+    let matchForm = matchPath(location.pathname, {
+      path: [genderUrl],
+      exact: false
+    });
 
     return (
       <Fragment>
@@ -173,7 +131,7 @@ class Form extends React.Component {
             <a 
               href="javascript:;"
               onClick={() => {
-                this.props.history.push(`${url}/gender`)
+                this.props.history.push(genderUrl)
               }}
             > Pick Gender </a>
           </p>
@@ -188,19 +146,28 @@ class Form extends React.Component {
           </p>
         </div>
 
-        {/* gender */}
-        <InnerPage
-          component={Gender}
-          path={`${url}/gender`}
-          exact={false}
-          location={this.props.location}
+        <TransitionGroup>
+          <CSSTransition 
+            key={matchForm ? 'form' : 'def'} 
+            classNames="inner-page" 
+            timeout={500}
+          > 
+            <Route 
+              location={this.props.location}
+              path={genderUrl} 
+              exact={false} 
+              render={props => (
+                <Gender {...props} 
+                  form={ this.state } 
+                  updateGender={(gender) => {
+                    this.setState({ usergender: gender });
+                  }} 
+                /> 
+              )}
+            />
+          </CSSTransition>
+        </TransitionGroup>
 
-          parentPath={url}
-          form={ this.state }
-          updateGender={(gender) => {
-            this.setState({ usergender: gender });
-          }} 
-        />
       </Fragment>
     );
     return <div className="app-page">from</div>
@@ -217,18 +184,27 @@ class PageA extends React.Component {
       usergender: ''
     }
   }
-  
+
+  componentDidMount() {
+    this.clickForm = false;
+  }
+
   render() {
-    let { route, match, location } = this.props;
-    let url = match.url;
-    let formUrl = `${url}/form`;
+    let { match, location } = this.props;
+    let formUrl = `${match.url}/form`;
     let matchForm = matchPath(location.pathname, {
-      path: formUrl,
+      path: [formUrl, `${formUrl}/gender`],
       exact: false
     });
 
+    // 不能直接访问子页面
+    if (matchForm && !this.clickForm) {
+      return <Redirect from={formUrl} to={match.url} push={false}/>
+    }
+
     return (
       <React.Fragment> 
+     
         <div className="app-page app-page-a">
           <p>
             Page A Data: { JSON.stringify(this.state) }
@@ -237,25 +213,34 @@ class PageA extends React.Component {
             <a 
               href="javascript:;"
               onClick={() => {
+                this.clickForm = true;
                 this.props.history.push(formUrl)
               }}
             > Edit User </a>
           </p>
         </div>
 
-        {/* form */}
-        <InnerPage
-          component={Form}
-          path={formUrl}
-          exact={false}
-          location={location}
-
-          parentPath={url}
-          form={ this.state }
-          updateUser={(user) => {
-            this.setState({ username: user.username, usergender: user.usergender });
-          }} 
-        />
+        <TransitionGroup>
+          <CSSTransition 
+            key={matchForm ? 'form' : 'def'} 
+            classNames="inner-page" 
+            timeout={500}
+          > 
+            <Route 
+              location={this.props.location}
+              path={formUrl} 
+              exact={false} 
+              render={props => (
+                <Form {...props} 
+                  form={ this.state } 
+                  updateUser={(user) => {
+                    this.setState({ username: user.username, usergender: user.usergender });
+                  }} 
+                /> 
+              )}
+            />
+          </CSSTransition>
+        </TransitionGroup>
       </React.Fragment>
     );
   }
@@ -268,9 +253,48 @@ class PageBDetail extends React.Component {
   }
   
   render() {
+    let { match, location } = this.props;
+    let url = match.url;
+    let matchModal = matchPath(location.pathname, {
+      path: [`${url}/modal`],
+      exact: false
+    });
+
+    const mymodal = (
+      <div className="app-modal">
+        <p>
+          { this.props.match.params.id }
+        </p>
+        <p>
+          <a 
+            href="javascript:;"
+            onClick={() => {
+              this.props.history.goBack()
+            }}
+          > Back </a>
+        </p>
+      </div>
+    );
+
     return (
-      <div className="app-page app-page-c-detail">
-        The Detail Id: { this.props.match.params.id }
+      <div className="app-page app-page-b-detail">
+        <p>
+          { this.props.match.params.id }
+        </p>
+
+        <p>
+          <Link to={`${url}/modal`}> Modal </Link>
+        </p>
+
+        <TransitionGroup component={null}>
+          <CSSTransition 
+            key={matchModal ? 'modal' : 'def'} 
+            classNames="modal" 
+            timeout={500}
+          > 
+           { matchModal ? mymodal : <div></div> }
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     );
   }
@@ -286,17 +310,54 @@ class PageB extends React.Component {
   }
   
   render() {
+    let { match, location } = this.props;
+    let url = match.url;
+    let matchModal = matchPath(location.pathname, {
+      path: [`${url}/modal`],
+      exact: false
+    });
+    
     const users = this.state.users.map(item => {
       return (
         <p key={item}>
-          <Link to={`/b/${item}`}> View User Detail: { item } </Link>
+          <Link to={`${url}/${item}`}> View User Detail: { item } </Link>
         </p>
       );
     });
 
+    const mymodal = (
+      <div className="app-modal">
+        <p>
+          modal
+        </p>
+        <p>
+          <a 
+            href="javascript:;"
+            onClick={() => {
+              this.props.history.goBack()
+            }}
+          > Back </a>
+        </p>
+      </div>
+    );
+
     return (
       <div className="app-page app-page-b">
+        <p>
+          <Link to={`${url}/modal`}> Modal </Link>
+        </p>
+
         { users }
+
+        <TransitionGroup component={null}>
+          <CSSTransition 
+            key={matchModal ? 'modal' : 'def'} 
+            classNames="modal" 
+            timeout={500}
+          > 
+           { matchModal ? mymodal : <div></div> }
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     );
   }
@@ -312,36 +373,39 @@ class PageC extends React.Component {
   render() {
     let url = this.props.match.url;
 
-    const MyModal = ({match, history}) => {
-      return (
-        <Modal 
-          ariaHideApp={false}
-          isOpen={!!match}
-          shouldCloseOnOverlayClick={false}
-        >
-          <p>Modal text!</p>
-          <button onClick={() => history.goBack()}>
-            Close Modal
-          </button>
-        </Modal>
-      );
-    }
-
     return (
       <div className="app-page app-page-c">
         <div>
           <a href="javascript:;" onClick={() => {
-            this.props.history.push(`${url}/mymodal`);
+            this.props.history.push(`${url}/modal`);
           }}> Show Modal </a>
         </div>
 
-        <Route path={`${url}/mymodal`} children={(props) => {
-          return <MyModal {...props} /> 
-        }}/>
+        <Route path={`${url}/modal`} children={({match, history}) => (
+          <Modal 
+            ariaHideApp={false}
+            isOpen={!!match}
+            shouldCloseOnOverlayClick={false}
+          >
+            <p>Modal text!</p>
+            <button onClick={() => history.goBack()}>
+              Close Modal
+            </button>
+          </Modal>
+        )}/>
       </div>
     );
   }
 }
+
+// nav link
+const NavLink = ({ to, ...rest }) => (
+  <Route path={to} children={({ match }) => (
+    <li className={match ? 'active' : ''}>
+      <Link to={to} {...rest}>{to}</Link>
+    </li>
+  )}/>
+);
 
 // app
 class App extends React.Component {
@@ -353,71 +417,75 @@ class App extends React.Component {
       name: 'a',
       path: '/a',
       childPaths: ['/a/form', '/a/form/gender'],
-      component: PageA
+      component: PageA,
+      extra: { strict: false, sensitive: true }
     }, {
       name: 'b',
       path: '/b',
       childPaths: ['/b/modal'],
-      component: PageB
+      component: PageB,
+      extra: { strict: false, sensitive: true }
     }, {
       name: 'b-dtl',
       path: '/b/:id',
-      childPaths: ['/b/:id/gender'],
-      component: PageBDetail
+      childPaths: ['/b/:id/modal'],
+      component: PageBDetail,
+      extra: { strict: false, sensitive: true }
     }, {
       name: 'c',
       path: '/c',
       childPaths: ['/c/modal'],
-      component: PageC
+      component: PageC,
+      extra: { strict: false, sensitive: true }
     }];
   }
   
   render() {
-    const NavLink = ({ to, ...rest }) => (
-      <Route path={to} children={({ match }) => (
-        <li className={match ? 'active' : ''}>
-          <Link to={to} {...rest}>{to}</Link>
-        </li>
-      )}/>
-    );
-
-    let transitionKey = getFirstUrlFromRoutes(this.routes, this.props.location);
-    console.log(transitionKey);
+    let matchedRoute = getMatchedRoute(this.routes, this.props.location);
+    let route = matchedRoute.route;
 
     return (
       <Fragment>
+        {/* navs */}
         <ul className="app-navs">
           <NavLink to='/a' />
           <NavLink to='/b' />
           <NavLink to='/c' />
         </ul>
+
+        {/* pages */}
         <TransitionGroup className="app-pages">
-          <CSSTransition key={transitionKey} classNames="entry-page" timeout={500}>
-            <Switch location={this.props.location}>
-              {
-                this.routes.map(item => {
-                  return <Route key={item.path} path={item.path} component={item.component} exact={false} />
-                })
-              }
-              <Route render={() => {
-                return (
-                  <div className="app-page">
-                    <p>
-                      React Router H5 Demo
-                    </p>
-                    <p>
-                      '/a': 子页面层层递进，不能单独访问
-                    </p>
-                    <p>
-                      '/b': 子页面单独访问
-                    </p>
-                    <p>
-                      '/c': 路由弹窗
-                    </p>
-                  </div>
-                )
-              }} exact={false}/>
-            </Switch>
+          <CSSTransition 
+            key={matchedRoute.key || 'def'} 
+            classNames="entry-page" 
+            timeout={500}
+          >
+            {
+              matchedRoute.key
+              ? 
+              <Route 
+                location={this.props.location}
+                path={route.path} 
+                component={route.component} 
+                exact={false} 
+                {...(route.extra || {})} 
+              />
+              : 
+              <div className="app-page">
+                <p>
+                  React Router H5 Demo
+                </p>
+                <p>
+                  '/a': 子页面层层递进，不能单独访问
+                </p>
+                <p>
+                  '/b': 子页面单独访问
+                </p>
+                <p>
+                  '/c': 路由弹窗
+                </p>
+              </div>
+            }
           </CSSTransition>
         </TransitionGroup>
       </Fragment>
